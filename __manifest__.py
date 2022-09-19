4cb661a226bcd7b7714e466da05647c010b5618b
MrTango <md@derico.de>
2022-09-19 19:48:14 +0200

    add SewageTreatmentPlant/SewageTreatmentPlantLocation

--- __manifest__.py
+++ __manifest__.py
@@ -23,7 +23,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/dtp_security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
