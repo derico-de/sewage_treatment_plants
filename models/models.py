81266a67a7c9992f88edc4043de1e80225d65fbb
MrTango <md@derico.de>
2022-09-19 19:57:11 +0200

    fix error message

--- models/models.py
+++ models/models.py
@@ -45,7 +45,7 @@ class ResPartnerBankAccMultiPartner(models.Model):
 
 
     _sql_constraints = [
-        ('unique_number', 'unique(sanitized_acc_number, company_id, partner_id)', 'Account Number must be unique2'),
+        ('unique_number', 'unique(sanitized_acc_number, company_id, partner_id)', 'Account Number must be unique'),
     ]
 
 class PartnerAzvLk(models.Model):
